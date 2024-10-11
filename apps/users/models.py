d121be1736014af1876a188b0213e8796f4c931a
xolmomin <xolmomin@gmail.com>
2024-10-11 17:13:45 +0500

    add tasks

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db.models import ManyToManyField, CASCADE, ForeignKey
 
 
 class User(AbstractUser):
     pass
+    # wishlist = ManyToManyField('shops.Book', CASCADE, blank=True)
